080ea46f63c42fecc9b68877b9e545b77e076daa
Keith Hatton <keith.hatton@ft.com>
2017-10-04 13:43:17 +0100

    :bug: Fix up mocks in test cases.

--- service/messageHandler_test.go
+++ service/messageHandler_test.go
@@ -40,6 +40,10 @@ func (p *mockMessageProducer) ConnectivityCheck() error {
 	return nil
 }
 
+func (p *mockMessageProducer) Shutdown() {
+
+}
+
 func TestMessageMapped(t *testing.T) {
 	whitelist := regexp.MustCompile(strings.Replace(testSystemId, ".", `\.`, -1))
 	mp := &mockMessageProducer{}
