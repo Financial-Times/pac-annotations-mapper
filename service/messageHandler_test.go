db2b7c1ca1010a0870544a3571b6dc067f6e7a30
Ivan Nikolov <ivan.nikolov@ft.com>
2019-11-01 12:09:23 +0200

    Refactor mapper tests

--- service/messageHandler_test.go
+++ service/messageHandler_test.go
@@ -10,23 +10,15 @@ import (
 
 	logTest "github.com/Financial-Times/go-logger/test"
 	"github.com/Financial-Times/kafka-client-go/kafka"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 const (
-	testSystemId = "http://cmdb.ft.com/systems/test-system"
-	testTxId     = "tid_testing"
-
-	hasBrand               = "http://www.ft.com/ontology/classification/isClassifiedBy"
-	mentions               = "http://www.ft.com/ontology/annotation/mentions"
-	implicitlyClassifiedBy = "http://www.ft.com/ontology/implicitlyClassifiedBy"
-	hasAuthor              = "http://www.ft.com/ontology/annotation/hasAuthor"
-	hasContributor         = "http://www.ft.com/ontology/hasContributor"
-	about                  = "http://www.ft.com/ontology/annotation/about"
-	hasDisplayTag          = "http://www.ft.com/ontology/hasDisplayTag"
+	testSystemID = "http://cmdb.ft.com/systems/test-system"
+	testTxID     = "tid_testing"
 )
 
 type mockMessageProducer struct {
@@ -48,202 +40,111 @@ func (p *mockMessageProducer) Shutdown() {
 
 }
 
-func TestMessageMapped(t *testing.T) {
+func TestMessageMappedNew(t *testing.T) {
 	hook := logTest.NewTestHook("test")
-	whitelist := regexp.MustCompile(strings.Replace(testSystemId, ".", `\.`, -1))
-	mp := &mockMessageProducer{}
-	mp.On("SendMessage", mock.AnythingOfType("kafka.FTMessage")).Return(nil)
+	whitelist := regexp.MustCompile(strings.Replace(testSystemID, ".", `\.`, -1))
 
-	service := NewAnnotationMapperService(whitelist, mp)
-
-	contentUuid := uuid.NewV4().String()
-	brandUuid := uuid.NewV4().String()
-	mentionsUuid := uuid.NewV4().String()
-	implicitlyClassifiedByUuid := uuid.NewV4().String()
-	hasAuthorUuid := uuid.NewV4().String()
-	hasContributorUuid := uuid.NewV4().String()
-	aboutUuid := uuid.NewV4().String()
-	hasDisplayTagUuid := uuid.NewV4().String()
-
-	inbound := kafka.FTMessage{
-		Headers: map[string]string{
-			"Origin-System-Id": testSystemId,
-			"X-Request-Id":     testTxId,
+	contentUUID := uuid.NewV4().String()
+	tests := map[string]struct {
+		PredicateURI    string
+		PredicateMapped string
+		ShouldSkip      bool
+	}{
+		"isClassifiedBy": {
+			PredicateURI:    "http://www.ft.com/ontology/classification/isClassifiedBy",
+			PredicateMapped: "isClassifiedBy",
 		},
-		Body: fmt.Sprintf(`{
-		"uuid":"%s",
-		"submittedBy":"test-user",
-		"annotations":[
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    }
-		]
-		}`, contentUuid,
-			hasBrand, brandUuid,
-			implicitlyClassifiedBy, implicitlyClassifiedByUuid,
-			hasAuthor, hasAuthorUuid,
-			hasContributor, hasContributorUuid,
-			about, aboutUuid,
-			hasDisplayTag, hasDisplayTagUuid,
-			mentions, mentionsUuid),
-	}
-
-	err := service.HandleMessage(inbound)
-	assert.NoError(t, err)
-
-	mp.AssertExpectations(t)
-
-	require.Len(t, mp.received, 1, "messages sent to producer")
-
-	actual := mp.received[0]
-	assert.Equal(t, testTxId, actual.Headers["X-Request-Id"], "transaction_id should be propagated")
-
-	actualBody := MappedAnnotations{}
-	json.NewDecoder(strings.NewReader(actual.Body)).Decode(&actualBody)
-
-	assert.Equal(t, contentUuid, actualBody.UUID, "content uuid")
-
-	actualAnnotations := actualBody.Annotations
-	assert.Len(t, actualAnnotations, 7, "annotations")
-
-	foundBrand := false
-	foundMentions := false
-	foundImplicitlyClassifiedBy := false
-	foundHasAuthor := false
-	foundHasContributor := false
-	foundAbout := false
-	foundHasDisplayTag := false
-
-	for _, ann := range actualAnnotations {
-		switch ann.Concept.Predicate {
-		case "isClassifiedBy":
-			assert.Equal(t, brandUuid, ann.Concept.ID, "brand annotation")
-			foundBrand = true
-
-		case "mentions":
-			assert.Equal(t, mentionsUuid, ann.Concept.ID, "mentions annotation")
-			foundMentions = true
-
-		case "implicitlyClassifiedBy":
-			assert.Equal(t, implicitlyClassifiedByUuid, ann.Concept.ID, "implicitlyClassifiedBy annotation")
-			foundImplicitlyClassifiedBy = true
-
-		case "hasAuthor":
-			assert.Equal(t, hasAuthorUuid, ann.Concept.ID, "hasAuthor annotation")
-			foundHasAuthor = true
-
-		case "hasContributor":
-			assert.Equal(t, hasContributorUuid, ann.Concept.ID, "hasContributor annotation")
-			foundHasContributor = true
-
-		case "about":
-			assert.Equal(t, aboutUuid, ann.Concept.ID, "about annotation")
-			foundAbout = true
-
-		case "hasDisplayTag":
-			assert.Equal(t, hasDisplayTagUuid, ann.Concept.ID, "hasDisplayTag annotation")
-			foundHasDisplayTag = true
-		}
-
-	}
-	assert.True(t, foundBrand, "expected brand predicate was not found")
-	assert.True(t, foundMentions, "expected mentions predicate was not found")
-	assert.True(t, foundImplicitlyClassifiedBy, "expected implicitlyClassifiedBy predicate was not found")
-	assert.True(t, foundHasAuthor, "expected hasAuthor predicate was not found")
-	assert.True(t, foundHasContributor, "expected hasContributor predicate was not found")
-	assert.True(t, foundAbout, "expected about predicate was not found")
-	assert.True(t, foundHasDisplayTag, "expected hasDisplayTag predicate was not found")
-
-	// check of monitoring logging
-	logTest.Assert(t, hook.LastEntry()).
-		HasMonitoringEvent("Map", testTxId, "Annotations").
-		HasValidFlag(true).
-		HasUUID(contentUuid)
-}
-
-func TestPredicateValidation(t *testing.T) {
-	hook := logTest.NewTestHook("test")
-	whitelist := regexp.MustCompile(strings.Replace(testSystemId, ".", `\.`, -1))
-	mp := &mockMessageProducer{}
-	mp.On("SendMessage", mock.AnythingOfType("kafka.FTMessage")).Return(nil)
-
-	service := NewAnnotationMapperService(whitelist, mp)
-
-	contentUuid := uuid.NewV4().String()
-	brandUuid := uuid.NewV4().String()
-	mentionsUuid := uuid.NewV4().String()
-	inbound := kafka.FTMessage{
-		Headers: map[string]string{
-			"Origin-System-Id": testSystemId,
-			"X-Request-Id":     testTxId,
+		"mentions": {
+			PredicateURI:    "http://www.ft.com/ontology/annotation/mentions",
+			PredicateMapped: "mentions",
 		},
-		Body: fmt.Sprintf(`{
-		"uuid":"%s",
-		"submittedBy":"test-user",
-		"annotations":[
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    },
-		    {
-		        "predicate":"%s",
-		        "id":"%s"
-		    }
-		]
-		}`, contentUuid,
-			hasBrand, brandUuid,
-			"http:///www.example.com/annotation/mentions", mentionsUuid),
+		"implicitlyClassifiedBy": {
+			PredicateURI:    "http://www.ft.com/ontology/implicitlyClassifiedBy",
+			PredicateMapped: "implicitlyClassifiedBy",
+		},
+		"hasAuthor": {
+			PredicateURI:    "http://www.ft.com/ontology/annotation/hasAuthor",
+			PredicateMapped: "hasAuthor",
+		},
+		"hasContributor": {
+			PredicateURI:    "http://www.ft.com/ontology/hasContributor",
+			PredicateMapped: "hasContributor",
+		},
+		"about": {
+			PredicateURI:    "http://www.ft.com/ontology/annotation/about",
+			PredicateMapped: "about",
+		},
+		"hasDisplayTag": {
+			PredicateURI:    "http://www.ft.com/ontology/hasDisplayTag",
+			PredicateMapped: "hasDisplayTag",
+		},
+		"invalid-predicate": {
+			PredicateURI: "invalid predicate",
+			ShouldSkip:   true,
+		},
 	}
 
-	err := service.HandleMessage(inbound)
-	assert.NoError(t, err)
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			mp := &mockMessageProducer{}
+			mp.On("SendMessage", mock.AnythingOfType("kafka.FTMessage")).Return(nil)
 
-	mp.AssertExpectations(t)
+			service := NewAnnotationMapperService(whitelist, mp)
 
-	require.Len(t, mp.received, 1, "messages sent to producer")
+			annotationID := uuid.NewV4().String()
+			inbound := kafka.FTMessage{
+				Headers: map[string]string{
+					"Origin-System-Id": testSystemID,
+					"X-Request-Id":     testTxID,
+				},
+				Body: fmt.Sprintf(`{
+				"uuid":"%s",
+				"submittedBy":"test-user",
+				"annotations":[
+					{
+						"predicate":"%s",
+						"id":"%s"
+					}
+				]
+				}`, contentUUID, test.PredicateURI, annotationID),
+			}
+			err := service.HandleMessage(inbound)
+			assert.NoError(t, err)
+			mp.AssertExpectations(t)
+			require.Len(t, mp.received, 1, "messages sent to producer")
 
-	actual := mp.received[0]
-	assert.Equal(t, testTxId, actual.Headers["X-Request-Id"], "transaction_id should be propagated")
+			actual := mp.received[0]
+			assert.Equal(t, testTxID, actual.Headers["X-Request-Id"], "transaction_id should be propagated")
 
-	actualBody := MappedAnnotations{}
-	json.NewDecoder(strings.NewReader(actual.Body)).Decode(&actualBody)
+			actualBody := MappedAnnotations{}
+			err = json.NewDecoder(strings.NewReader(actual.Body)).Decode(&actualBody)
+			assert.NoError(t, err)
 
-	assert.Equal(t, contentUuid, actualBody.UUID, "content uuid")
+			assert.Equal(t, contentUUID, actualBody.UUID, "content uuid")
 
-	actualAnnotations := actualBody.Annotations
-	assert.Len(t, actualAnnotations, 1, "annotations")
-	assert.Equal(t, brandUuid, actualAnnotations[0].Concept.ID, "brand annotation")
+			actualAnnotations := actualBody.Annotations
+			if test.ShouldSkip {
+				if len(actualAnnotations) != 0 {
+					t.Fatal("did not expect to map annotation")
+				}
+				return
+			}
 
-	// check of monitoring logging
+			if len(actualAnnotations) != 1 {
+				t.Fatal("expected one annotation to be mapped")
+			}
+
+			annotation := actualAnnotations[0]
+			assert.Equal(t, test.PredicateMapped, annotation.Concept.Predicate)
+			assert.Equal(t, annotationID, annotation.Concept.ID)
+
+		})
+	}
 	logTest.Assert(t, hook.LastEntry()).
-		HasMonitoringEvent("Map", testTxId, "Annotations").
+		HasMonitoringEvent("Map", testTxID, "Annotations").
 		HasValidFlag(true).
-		HasUUID(contentUuid)
+		HasUUID(contentUUID)
+
 }
 
 func TestSourceNonMatchingWhitelistIsIgnored(t *testing.T) {
@@ -252,7 +153,7 @@ func TestSourceNonMatchingWhitelistIsIgnored(t *testing.T) {
 	mp := &mockMessageProducer{}
 	service := NewAnnotationMapperService(whitelist, mp)
 	inbound := kafka.FTMessage{
-		Headers: map[string]string{"Origin-System-Id": testSystemId},
+		Headers: map[string]string{"Origin-System-Id": testSystemID},
 		Body:    `{"foo":"bar"}`,
 	}
 
@@ -263,11 +164,11 @@ func TestSourceNonMatchingWhitelistIsIgnored(t *testing.T) {
 
 func TestSyntacticallyInvalidJsonIsRejected(t *testing.T) {
 	hook := logTest.NewTestHook("test")
-	whitelist := regexp.MustCompile(strings.Replace(testSystemId, ".", `\.`, -1))
+	whitelist := regexp.MustCompile(strings.Replace(testSystemID, ".", `\.`, -1))
 	mp := &mockMessageProducer{}
 	service := NewAnnotationMapperService(whitelist, mp)
 	inbound := kafka.FTMessage{
-		Headers: map[string]string{"Origin-System-Id": testSystemId},
+		Headers: map[string]string{"Origin-System-Id": testSystemID},
 		Body:    `{"foo":"bar"`,
 	}
 
@@ -284,17 +185,17 @@ func TestSyntacticallyInvalidJsonIsRejected(t *testing.T) {
 func TestMessageProducerError(t *testing.T) {
 	hook := logTest.NewTestHook("test")
 	errmsg := "test error"
-	whitelist := regexp.MustCompile(strings.Replace(testSystemId, ".", `\.`, -1))
+	whitelist := regexp.MustCompile(strings.Replace(testSystemID, ".", `\.`, -1))
 	mp := &mockMessageProducer{}
 	mp.On("SendMessage", mock.AnythingOfType("kafka.FTMessage")).Return(errors.New(errmsg))
 
 	service := NewAnnotationMapperService(whitelist, mp)
 
-	contentUuid := uuid.NewV4().String()
+	contentUUID := uuid.NewV4().String()
 	inbound := kafka.FTMessage{
 		Headers: map[string]string{
-			"Origin-System-Id": testSystemId,
-			"X-Request-Id":     testTxId,
+			"Origin-System-Id": testSystemID,
+			"X-Request-Id":     testTxID,
 		},
 		Body: fmt.Sprintf(`{
 		"uuid":"%s",
@@ -305,7 +206,7 @@ func TestMessageProducerError(t *testing.T) {
 		        "id":"bar"
 		    }
 		]
-		}`, contentUuid),
+		}`, contentUUID),
 	}
 
 	err := service.HandleMessage(inbound)
@@ -315,9 +216,9 @@ func TestMessageProducerError(t *testing.T) {
 
 	// check of monitoring logging
 	logTest.Assert(t, hook.LastEntry()).
-		HasMonitoringEvent("Map", testTxId, "Annotations").
+		HasMonitoringEvent("Map", testTxID, "Annotations").
 		HasValidFlag(true).
-		HasUUID(contentUuid)
+		HasUUID(contentUUID)
 }
 
 func TestNilWhitelistIsIgnoredWithErrorLog(t *testing.T) {
@@ -325,7 +226,7 @@ func TestNilWhitelistIsIgnoredWithErrorLog(t *testing.T) {
 	mp := &mockMessageProducer{}
 	service := NewAnnotationMapperService(nil, mp)
 	inbound := kafka.FTMessage{
-		Headers: map[string]string{"Origin-System-Id": testSystemId},
+		Headers: map[string]string{"Origin-System-Id": testSystemID},
 		Body:    `{"foo":"bar"}`,
 	}
 
